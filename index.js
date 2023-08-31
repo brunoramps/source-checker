3c1623f3858b060ccc75af2c4bf67406a75394f1
Bruno Ramos <62392117+brunoramps@users.noreply.github.com>
2023-08-30 21:16:01 -0300

    subindo pra continuar no note do trabalho

---
+++ index.js
@@ -0,0 +1,36 @@
+const { main } = require('./sendmail');
+
+//main('nome-do-arquivo') -> este é o nome do arquivo sem a extensão .csv, localizado em /arquivos/nome-do-arquivo.csv
+
+
+main('viaaroma');
+
+/* analisar
+*/
+
+
+/* Analisado
+main('daikin');
+main('uplips');
+main('newwhite');
+main('vivabeauty');
+main('desincha');
+main('100peso');
+main('matisse1');
+main('matisse2');
+main('matisse3');
+main('matisse4');
+main('matisse5');
+main('matisse6');
+main('matisse7');
+main('matisse8');
+main('matisse9');
+main('matisse10');
+main('matisse11');
+
+*/
+
+/* Arquivados
+main('dss');
+
+*/
--- sendmail.js
+++ sendmail.js
@@ -4,12 +4,17 @@ const csvParser = require('csv-parser');
 const cheerio = require('cheerio');
 const axios = require('axios');
 const nodemailer = require('nodemailer');
+const moment = require('moment');
 
-async function main() {
+async function main(cliente) {
   const urls = [];
+  const today = moment().format('YYYY-MM-DD');
+  let qtdErros = 0;
+  let contador = 1;
+  let $ = "";
 
   // Lendo o arquivo CSV e coletando as URLs
-  fs.createReadStream('urls.csv')
+  fs.createReadStream(`arquivos/${cliente}.csv`)
     .pipe(csvParser())
     .on('data', (data) => {
       if (data.url) {
@@ -31,15 +36,25 @@ async function main() {
       // Acessando cada URL e coletando o título
       for (const url of urls) {
         try {
-          console.log(`[...] - ${url}`);
+          console.log(`${url}`);
+          console.log(`[Analisando ${contador++}/${urls.length}...]`);
           await page.goto(url, { waitUntil: 'networkidle0' });
           const title = await page.title();
-          const pageContent = await page.content();
 
-          const { data } = await axios.get(url);
+          try {
+            const { data } = await axios.get(url);
+            $ = cheerio.load(data);
+            
+          } catch (err) {
+            console.error("Error response:");
+            $ = cheerio.load(err.response.data);
+            //console.error(err.response.data);    // ***
+            //console.error(err.response.status);  // ***
+            //console.error(err.response.headers); // ***
+            
+          }
     
           // Load HTML we fetched in the previous line
-          const $ = cheerio.load(data);
 
           // Tag Title
           const titleSource = await $('title').text();
@@ -48,11 +63,21 @@ async function main() {
             url,
             pageTitle: title,
             titleSource,
-            titlesMatch: title === titleSource ? 'Sim' : 'Não',
+            titlesIguais: titlesMatch(title, titleSource)
           };
 
+          function titlesMatch(pageTitle, sourceTitle){
+            if(pageTitle === sourceTitle){
+              return "Sim"
+            }else{
+              qtdErros++;
+              return "Não"
+            }
+
+          }
+
           results.push(result);
-          console.log(`[OK] - ${url}`);
+          console.log(`[OK]`);
         } catch (error) {
           console.error(`Erro ao acessar a URL ${url}: ${error}`);
         }
@@ -62,11 +87,11 @@ async function main() {
       await browser.close();
 
       // Escrevendo os resultados em um novo arquivo CSV
-      const newCsvFilePath = 'results.csv';
+      const newCsvFilePath = `resultados/${cliente}.csv`;
       const newCsvStream = fs.createWriteStream(newCsvFilePath);
-      newCsvStream.write('URL,pageTitle,titleSource,Iguais\n');
+      newCsvStream.write('URL,pageTitle,titleSource,titlesIguais,Data\n');
       results.forEach((result) => {
-        newCsvStream.write(`${result.url},${result.pageTitle},${result.titleSource},${result.titlesMatch}\n`);
+        newCsvStream.write(`${result.url},${result.pageTitle},${result.titleSource},${result.titlesIguais},${today}\n`);
       });
       newCsvStream.end();
 
@@ -74,21 +99,67 @@ async function main() {
 
       // Enviando o arquivo CSV via e-mail
       const transporter = nodemailer.createTransport({
-        service: 'gmail', // Ex: 'gmail'
+        //service: 'gmail', // Ex: 'gmail'
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
           user: 'bruno.ramos@webart.com.br',
           pass: 'dpsotytmlpzepluu'
+        },
+        tls: {
+          rejectUnauthorized: false
         }
       });
 
+      let analistas = {
+        bruno : {
+          "email" : "bruno.ramos@webart.com.br"
+        },
+        hugo : {
+          email : "hugo.iwaasa@webart.com.br"
+        }
+      }
+
+      let emailAnalista = "";
+      switch (cliente) {
+        case "desincha":
+          emailAnalista = analistas.bruno.email
+          break;
+        case "daikin":
+          emailAnalista = analistas.hugo.email
+          break;
+        case "matisse":
+          emailAnalista = analistas.hugo.email
+          break;
+        case "100peso":
+          emailAnalista = analistas.hugo.email
+          break;
+        case "newwhite":
+          emailAnalista = analistas.hugo.email
+          break;
+        case "uplips":
+          emailAnalista = analistas.hugo.email
+        case "vivabeauty":
+          emailAnalista = analistas.hugo.email
+          break;
+        case "viaaroma":
+          emailAnalista = analistas.bruno.email 
+        default:
+          break;
+      }
+      console.log("Email enviado para o analista: ", emailAnalista);
       const mailOptions = {
         from: 'bruno.ramos@webart.com.br',
         to: 'seo@webart.com.br',
-        subject: 'Resultados da Verificação de Títulos',
-        text: 'Aqui estão os resultados da verificação de títulos.',
+        cc: [
+          `${emailAnalista}`
+        ],
+        subject: `${qtdErros} erros encontrados na Verificação de Código-Fonte de ${cliente}`,
+        text: `Segue em anexo o arquivo .csv gerado pelo sistema com os dados da verificação`,
         attachments: [
           {
-            filename: 'results.csv',
+            filename: `${cliente}.csv`,
             path: newCsvFilePath
           }
         ]
@@ -104,4 +175,6 @@ async function main() {
     });
 }
 
-main();
+//main();
+
+exports.main = main;
